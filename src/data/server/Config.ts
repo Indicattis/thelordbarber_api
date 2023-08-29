554b1c2d1180b801fca43de9ccffdf7036ad2bc8
Indicattis <joao.staehler@gmail.com>
2023-08-29 13:13:10 -0300

    Update Config.ts

--- src/data/server/Config.ts
+++ src/data/server/Config.ts
@@ -1,11 +1,11 @@
 // AWS
-// export const serverUrl = 'https://thelordbarber-server.devliners.com.br'
+export const serverUrl = 'https://thelordbarber-server.devliners.com.br'
 
 // VERCEL
 // export const serverUrl = `https://thelordbarber-server-git-main-indicattis.vercel.app`
 
 // LOCALHOST
-export const serverUrl = '//localhost:9001'
+// export const serverUrl = '//localhost:9001'
 
 
 // https://wa.me/5554996598024/?text=textourl%asdas
